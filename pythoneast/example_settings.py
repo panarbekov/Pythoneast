ccb4a13ea4c18a888849432121c83453e51da41c
sultan <sultan.panar@gmail.com>
2020-11-11 20:37:59 +0600

    bla bla bla

--- pythoneast/example_settings.py
+++ pythoneast/example_settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3p_w4-%2)+=re73sh2k$h0vi%07viy7v%@bqi0x%offzkg&^7p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
